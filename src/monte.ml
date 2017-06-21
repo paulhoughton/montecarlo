b1b5bdb7d344fa81992aa123cbf1826d8d1ad2ab
paulhoughton <paulhoughton@users.noreply.github.com>
2017-06-21 08:00:59 +0100

    Formatting

--- src/monte.ml
+++ src/monte.ml
@@ -1,15 +1,22 @@
-Random.self_init();;
+Random.self_init()
 
 let calcValues investment avg vol years repeat callback =  
+
   let rec total running count =
-  if count = 0 then running
-  else let v = Ierfc.ppf avg vol (Random.float 1.) in
-    total (running *. (1.+.v)) (count-1) in
+    if count = 0 then
+      running
+    else 
+      let v = Ierfc.ppf avg vol (Random.float 1.) in
+      total (running *. (1.+.v)) (count-1) in
+
   let rec repeatTotal running minV maxV count =
-    if count = 0 then running, minV, maxV
-    else let v = total investment years in 
+    if count = 0 then
+      running, minV, maxV
+    else 
+      let v = total investment years in 
       let minV = min v minV in
       let maxV = max v maxV in
       if count mod (repeat / 100) = 0 then callback(101-(count*100)/repeat);
       repeatTotal (running +. v) minV maxV (count - 1) in
-        repeatTotal 0. infinity 0. repeat;;  
+
+  repeatTotal 0. infinity 0. repeat
